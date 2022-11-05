3a78d173c47ad94161e6de652d7586fb439bd5d8
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2022-11-05 23:22:32 +0100

    image not showing. hmmm.

--- src/Footer.js
+++ src/Footer.js
@@ -6,13 +6,13 @@ function Footer() {
             <div className="footer-contain">
                 <div className="footer-content">
                     <div className="zuri">
-                        <img src="./zuri.png" alt="" />
+                        <img src="zuri.png" alt="" />
                     </div>
                     <div className="stage">
                         <a href="/">HNG Internship 9 Frontend Task</a>
                     </div>
                     <div className="ingress">
-                        <img src="./i4G.png" alt="" className="ingress" />
+                        <img src="i4G.png" alt="" className="ingress" />
                     </div>
                 </div>
             </div>
